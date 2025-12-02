1ff73f0eb6092ebd5efba0a19dbf5fdc9cfc11d1
Yasin <yasinpeker43@gmail.com>
2025-12-02 09:40:00 +0300

    configuration
    postgresql docker is configured and user-secrets are set for the commit

--- src/CurrencyTracker.API/Program.cs
+++ src/CurrencyTracker.API/Program.cs
@@ -1,15 +1,25 @@
+using CurrencyTracker.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseNpgsql(connectionString);
+});
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddOpenApi();
+
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+  
     app.UseSwagger();
     app.UseSwaggerUI();
 }
---
+++ src/CurrencyTracker.Infrastructure/Persistence/AppDbContext.cs
@@ -0,0 +1,13 @@
+using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace CurrencyTracker.Infrastructure.Persistence;
+
+public class AppDbContext:DbContext
+{
+  public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+        
+    }
+    
+}
