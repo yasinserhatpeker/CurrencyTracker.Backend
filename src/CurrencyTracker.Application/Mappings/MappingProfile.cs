3a4736d444ad98b9980ec7f76f1ec180a0f3f7d0
Yasin <yasinpeker43@gmail.com>
2026-01-20 20:47:47 +0300

    MappingProfile is done for dto mapping.

---
+++ src/CurrencyTracker.Application/Mappings/MappingProfile.cs
@@ -0,0 +1,16 @@
+using System;
+using AutoMapper;
+using CurrencyTracker.Application.DTOs;
+using CurrencyTracker.Domain.Entities;
+
+namespace CurrencyTracker.Application.Mappings;
+
+public class MappingProfile : Profile
+{
+   public MappingProfile()
+    {
+        CreateMap<CreateUserDTO,User>().ForMember(dest => dest.PasswordHash, opt => opt.MapFrom(src => src.Password)); // CreateMap<Source,Destination>(); src-> password == dest-> passwordHash
+        CreateMap<User,UserResponseDTO>();
+
+    }
+}
