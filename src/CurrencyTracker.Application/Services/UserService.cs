255135483aa3a0939929c80626902833eaa1534f
Yasin <yasinpeker43@gmail.com>
2026-01-20 21:41:15 +0300

    fix typo

--- src/CurrencyTracker.Application/Services/UserService.cs
+++ src/CurrencyTracker.Application/Services/UserService.cs
@@ -1,11 +1,19 @@
 using System;
+using AutoMapper;
 using CurrencyTracker.Application.DTOs;
 using CurrencyTracker.Application.Interfaces;
 
 namespace CurrencyTracker.Application.Services;
 
 public class UserService : IUserService
-{
+{  
+    private readonly Mapper _mapper;
+    public UserService(Mapper mapper)
+    {
+        _mapper =mapper;
+    }
+    
+
      
     public Task CreateUserAsync(CreateUserDTO createUserDTO)
     {
