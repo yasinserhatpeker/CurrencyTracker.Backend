d4c4288af377becea9ed40989afd2d831b7bae59
Yasin <yasinpeker43@gmail.com>
2026-01-21 09:22:33 +0300

    portfolioDTO is added to the MappingProfile

--- src/CurrencyTracker.Application/Mappings/MappingProfile.cs
+++ src/CurrencyTracker.Application/Mappings/MappingProfile.cs
@@ -1,6 +1,7 @@
 using System;
 using AutoMapper;
 using CurrencyTracker.Application.DTOs;
+using CurrencyTracker.Application.DTOs.Portfolios;
 using CurrencyTracker.Domain.Entities;
 
 namespace CurrencyTracker.Application.Mappings;
@@ -12,7 +13,10 @@ public class MappingProfile : Profile
         CreateMap<CreateUserDTO,User>().ForMember(dest => dest.PasswordHash, opt => opt.MapFrom(src => src.Password)); // CreateMap<Source,Destination>(); src-> password == dest-> passwordHash
         CreateMap<User,UserResponseDTO>();
 
-        
+        CreateMap<CreatePortfolioDTO,Portfolio>();
+        CreateMap<Portfolio,PortfolioResponseDTO>();
+
+
 
     }
 }
