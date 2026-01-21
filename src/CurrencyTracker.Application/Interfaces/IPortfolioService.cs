40cd2a0ec7b009dd74249c2ac0914ec275775ca6
Yasin <yasinpeker43@gmail.com>
2026-01-21 19:22:39 +0300

    IPortfolioService is done for now.

--- src/CurrencyTracker.Application/Interfaces/IPortfolioService.cs
+++ src/CurrencyTracker.Application/Interfaces/IPortfolioService.cs
@@ -1,8 +1,14 @@
 using System;
+using CurrencyTracker.Application.DTOs.Portfolios;
 
 namespace CurrencyTracker.Application.Interfaces;
 
 public interface IPortfolioService
-{
-    
+{    
+    Task CreatePortfolioAsync(CreatePortfolioDTO createPortfolioDTO);
+    Task<PortfolioResponseDTO> GetByIdAsync(Guid id);
+    Task<IEnumerable<PortfolioResponseDTO>> GetAllPortfoliosAsync();
+    Task DeletePortfolioAsync(Guid id);
+    Task UpdatePortfolioAsync(UpdatePortfolioDTO updatePortfolioDTO,Guid id);
+
 }
