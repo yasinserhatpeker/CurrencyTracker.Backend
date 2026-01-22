d1250b05eb9660420ff54a1cc46fd47afd88fe5a
Yasin <yasinpeker43@gmail.com>
2026-01-22 10:54:32 +0300

    createTransactionDTO is done for now

--- src/CurrencyTracker.Application/DTOs/Transactions/CreateTransactionDTO.cs
+++ src/CurrencyTracker.Application/DTOs/Transactions/CreateTransactionDTO.cs
@@ -4,5 +4,9 @@ namespace CurrencyTracker.Application.DTOs.Transactions;
 
 public class CreateTransactionsDTO
 {
-
+    public Guid PortfolioId { get; set; }
+    public DateTime TransactionDate { get; set; }
+    public string Symbol { get; set; } = default!;
+    public string Price { get; set; } = default!;
+    public string Quantity { get; set; } = default!;
 }
