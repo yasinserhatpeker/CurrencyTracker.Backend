ad3b4b599373ea6b293d4f0acd3752d72a9e0f8f
Yasin <yasinpeker43@gmail.com>
2026-01-24 09:34:14 +0300

    refactoring transactionResponseDTO

--- src/CurrencyTracker.Application/DTOs/Transactions/TransactionResponseDTO.cs
+++ src/CurrencyTracker.Application/DTOs/Transactions/TransactionResponseDTO.cs
@@ -4,6 +4,10 @@ namespace CurrencyTracker.Application.DTOs.Transactions;
 
 public class TransactionResponseDTO
 {
+   public Guid Id {get;set;}
+   public Guid PortfolioId {get;set;} 
+   public decimal Price {get;set;}
+   public decimal Quantity {get;set;}
    public DateTime TransactionDate {get;set;} 
    public string Symbol {get;set;} = default!;
    public decimal TotalValue {get;set;}
