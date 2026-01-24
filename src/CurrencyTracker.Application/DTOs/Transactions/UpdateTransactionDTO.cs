77ebd078b1bc92ece8e71c7c31d72500a9f13f52
Yasin <yasinpeker43@gmail.com>
2026-01-24 09:41:27 +0300

    updateTransactionDTO is done for now

--- src/CurrencyTracker.Application/DTOs/Transactions/UpdateTransactionDTO.cs
+++ src/CurrencyTracker.Application/DTOs/Transactions/UpdateTransactionDTO.cs
@@ -1,8 +1,14 @@
-using System;
+using CurrencyTracker.Domain.Entities.Enums;
 
 namespace CurrencyTracker.Application.DTOs.Transactions;
 
 public class UpdateTransactionDTO
 {
-  
+  public Guid Id{get;set;}
+  public TransactionType TransactionType {get;set;}
+  public string Symbol {get;set;} = default!;
+  public decimal Price {get;set;} 
+  public decimal Quantity{get;set;}
+  public DateTime TransactionDate{get;set;}
+
 }
