d856eb2e275f5a90041e16370bd1266902fd3aec
Yasin <yasinpeker43@gmail.com>
2026-01-24 09:42:19 +0300

    adding PortfolioId to the updateTransactionDTO

--- src/CurrencyTracker.Application/DTOs/Transactions/UpdateTransactionDTO.cs
+++ src/CurrencyTracker.Application/DTOs/Transactions/UpdateTransactionDTO.cs
@@ -5,6 +5,7 @@ namespace CurrencyTracker.Application.DTOs.Transactions;
 public class UpdateTransactionDTO
 {
   public Guid Id{get;set;}
+  public Guid PortfolioId {get;set;}
   public TransactionType TransactionType {get;set;}
   public string Symbol {get;set;} = default!;
   public decimal Price {get;set;} 
