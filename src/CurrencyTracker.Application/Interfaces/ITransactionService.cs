4bf18380f0dbcc90c241987e0d43d1ab3a7053de
Yasin <yasinpeker43@gmail.com>
2026-01-24 09:57:32 +0300

    ITransactionService interface is done

--- src/CurrencyTracker.Application/Interfaces/ITransactionService.cs
+++ src/CurrencyTracker.Application/Interfaces/ITransactionService.cs
@@ -1,9 +1,13 @@
-using System;
 using CurrencyTracker.Application.DTOs.Transactions;
 
 namespace CurrencyTracker.Application.Interfaces;
 
 public interface ITransactionService
 {
-    Task<IEnumerable<TransactionResponseDTO>> GetAllTransactionsByUser(Guid userId);
+    Task CreateTransactionAsync(CreateTransactionsDTO createTransactionsDTO);
+    Task<IEnumerable<TransactionResponseDTO>> GetTransactionsByPortfolioAsync(Guid portfolioId);
+    Task<TransactionResponseDTO> GetByIdAsync(Guid Id);
+    Task DeleteTransactionAsync(Guid id);
+    Task UpdateTransactionAsync(Guid id, UpdateTransactionDTO updateTransactionDTO);
+
 }
