07b27181c50a44caff7be5a02be31fbf1743feb1
Yasin <yasinpeker43@gmail.com>
2026-01-24 10:02:35 +0300

    Dependency Injection is done for TransactionService

--- src/CurrencyTracker.Application/Services/TransactionService.cs
+++ src/CurrencyTracker.Application/Services/TransactionService.cs
@@ -1,14 +1,25 @@
 using System;
+using System.Transactions;
+using AutoMapper;
 using CurrencyTracker.Application.DTOs.Transactions;
 using CurrencyTracker.Application.Interfaces;
+using CurrencyTracker.Domain.Interfaces;
 
 namespace CurrencyTracker.Application.Services;
 
 public class TransactionService : ITransactionService
 {
-    public Task CreateTransactionAsync(CreateTransactionsDTO createTransactionsDTO)
+    private readonly IMapper _mapper;
+    private readonly IGenericRepository<Transaction> _transactionRepository;
+
+    public TransactionService(IMapper mapper, IGenericRepository<Transaction> transactionRepository)
     {
-        throw new NotImplementedException();
+        _mapper=mapper;
+        _transactionRepository=transactionRepository;
+    }
+    public async Task CreateTransactionAsync(CreateTransactionsDTO createTransactionsDTO)
+    {
+        
     }
 
     public Task DeleteTransactionAsync(Guid id)
