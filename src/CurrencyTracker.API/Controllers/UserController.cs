0e97ed2806cd0624b0b2397efafba0d597e458a0
Yasin <yasinpeker43@gmail.com>
2026-01-24 18:49:39 +0300

    UserController is created

---
+++ src/CurrencyTracker.API/Controllers/UserController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace CurrencyTracker.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        
+    }
+}
