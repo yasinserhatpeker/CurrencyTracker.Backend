5dad8f5af3236254e2a4bb8605f860f5e91e9c83
Yasin <yasinpeker43@gmail.com>
2026-01-26 10:28:19 +0300

    transactionController is created

---
+++ src/CurrencyTracker.API/Controllers/TransactionController.cs
@@ -0,0 +1,18 @@
+using CurrencyTracker.Application.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CurrencyTracker.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TransactionController : ControllerBase
+    {
+        private readonly ITransactionService _transactionService;
+        public TransactionController(ITransactionService transactionService)
+        {
+            _transactionService=transactionService;
+        }
+        
+    }
+}
