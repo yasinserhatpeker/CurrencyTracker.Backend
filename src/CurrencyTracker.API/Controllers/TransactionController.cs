1020230c9918bb7fd476808c3b14d965633a6567
Yasin <yasinpeker43@gmail.com>
2026-01-26 10:38:19 +0300

    create endpoint is done

--- src/CurrencyTracker.API/Controllers/TransactionController.cs
+++ src/CurrencyTracker.API/Controllers/TransactionController.cs
@@ -1,3 +1,4 @@
+using CurrencyTracker.Application.DTOs.Transactions;
 using CurrencyTracker.Application.Interfaces;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -13,6 +14,24 @@ namespace CurrencyTracker.API.Controllers
         {
             _transactionService=transactionService;
         }
-        
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateTransactionsDTO createTransactionsDTO)
+        {
+            if(!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+               await _transactionService.CreateTransactionAsync(createTransactionsDTO);
+               return Ok(new{message ="Transaction created succesfully!"});
+            }
+            catch(Exception ex)
+            {
+                return BadRequest (new {message = ex.Message});
+            }
+
+        }
     }
 }
