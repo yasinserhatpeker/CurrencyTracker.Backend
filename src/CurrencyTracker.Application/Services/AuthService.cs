5c4c937f22bd40bc3fc065248ac45c251e7cfd8f
Yasin <yasinpeker43@gmail.com>
2026-01-28 16:58:49 +0300

    JwtSettings are added to the appsettings

--- src/CurrencyTracker.Application/Services/AuthService.cs
+++ src/CurrencyTracker.Application/Services/AuthService.cs
@@ -1,8 +1,32 @@
 using System;
+using AutoMapper;
+using CurrencyTracker.Application.DTOs;
+using CurrencyTracker.Application.DTOs.Auth;
+using CurrencyTracker.Application.DTOs.Users;
+using CurrencyTracker.Application.Interfaces;
+using CurrencyTracker.Domain.Entities;
+using CurrencyTracker.Domain.Interfaces;
 
 namespace CurrencyTracker.Application.Services;
 
-public class AuthService
+public class AuthService : IAuthService
 {
+     private readonly IMapper _mapper;
+     private readonly IGenericRepository<User> _userRepository;
+     
+     private readonly IConfiguration _configuration;
+    public Task<AuthResponseDTO> LoginUserAsync(LoginUserDTO loginUserDTO)
+    {
+        throw new NotImplementedException();
+    }
 
+    public Task<AuthResponseDTO> RefreshTokenAsync(string RefreshToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<UserResponseDTO> RegisterUserAsync(CreateUserDTO createUserDTO)
+    {
+        throw new NotImplementedException();
+    }
 }
