d1b6ca20d4774a90070be3ccfd26ebc2395a62ea
Yasin <yasinpeker43@gmail.com>
2026-01-28 16:43:49 +0300

    access and refresh tokens are created in AuthResponseDTO

--- src/CurrencyTracker.Application/DTOs/Auth/AuthResponseDTO.cs
+++ src/CurrencyTracker.Application/DTOs/Auth/AuthResponseDTO.cs
@@ -4,5 +4,6 @@ namespace CurrencyTracker.Application.DTOs.Auth;
 
 public class AuthResponseDTO
 {
-
+   public string AccessToken {get;set;} = default!;
+   public string RefreshToken {get;set;} = default!;
 }
