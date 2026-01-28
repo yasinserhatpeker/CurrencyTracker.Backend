dbd0e95442669a8d064b742f871df395fb538124
Yasin <yasinpeker43@gmail.com>
2026-01-28 11:10:18 +0300

    migrations and database update is done for refresh token and authprovider

--- src/CurrencyTracker.Domain/Entities/User.cs
+++ src/CurrencyTracker.Domain/Entities/User.cs
@@ -4,9 +4,12 @@ namespace CurrencyTracker.Domain.Entities;
 
 public class User : BaseEntity
 {
-    public string Username {get; set;} = string.Empty;
-    public string PasswordHash {get; set;} = string.Empty;
-    public string Email {get; set;} = string.Empty;
-    public ICollection<Portfolio> Portfolios {get; set;} = new List<Portfolio>();
+    public string Username { get; set; } = string.Empty;
+    public string PasswordHash { get; set; } = string.Empty;
+    public string AuthProvider { get; set; } = "Local";
+    public string? RefreshToken { get; set; }
+    public DateTime? RefreshTokenExpiryTime { get; set; }
+    public string Email { get; set; } = string.Empty;
+    public ICollection<Portfolio> Portfolios { get; set; } = new List<Portfolio>();
 
 }
---
+++ src/CurrencyTracker.Infrastructure/Migrations/20260128080408_AddRefreshToken.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CurrencyTracker.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRefreshToken : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AuthProvider",
+                table: "Users",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "RefreshToken",
+                table: "Users",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "RefreshTokenExpiryTime",
+                table: "Users",
+                type: "timestamp with time zone",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AuthProvider",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "RefreshToken",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "RefreshTokenExpiryTime",
+                table: "Users");
+        }
+    }
+}
