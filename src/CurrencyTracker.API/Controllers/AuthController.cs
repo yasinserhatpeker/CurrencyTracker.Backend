1d9047963404c29e85ecf89ba4dc2dc57438451a
Yasin <yasinpeker43@gmail.com>
2026-01-30 09:41:37 +0300

    authController is created.

---
+++ src/CurrencyTracker.API/Controllers/AuthController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CurrencyTracker.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        
+    }
+}
