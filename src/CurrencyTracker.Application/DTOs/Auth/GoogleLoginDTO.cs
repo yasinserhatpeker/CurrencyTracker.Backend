0f34ee9699bec48fbf85798e9129d71e9def628c
Yasin <yasinpeker43@gmail.com>
2026-02-14 22:00:44 +0300

    googleLoginDto is created

---
+++ src/CurrencyTracker.Application/DTOs/Auth/GoogleLoginDTO.cs
@@ -0,0 +1,9 @@
+using System;
+using System.Runtime.CompilerServices;
+
+namespace CurrencyTracker.Application.DTOs.Auth;
+
+public class GoogleLoginDTO
+{
+   public string IdToken {get;set;} = default!;
+}
