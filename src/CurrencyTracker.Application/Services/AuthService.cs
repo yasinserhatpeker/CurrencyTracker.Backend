cf0a6492172093fd861b89b7375fcf22ef5d477c
Yasin <yasinpeker43@gmail.com>
2026-02-15 11:32:59 +0300

    fix response type for googleatuh

--- src/CurrencyTracker.Application/Services/AuthService.cs
+++ src/CurrencyTracker.Application/Services/AuthService.cs
@@ -120,4 +120,8 @@ public class AuthService : IAuthService
         }
     }
 
+    public Task<AuthResponseDTO> GoogleLoginAsync(GoogleLoginDTO googleLoginDTO)
+    {
+        throw new NotImplementedException();
+    }
 }
