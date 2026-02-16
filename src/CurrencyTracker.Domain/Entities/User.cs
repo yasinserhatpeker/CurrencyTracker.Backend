c5d5afb6a8491f54f477eb33ff55fb3fe8cf9f2b
Yasin <yasinpeker43@gmail.com>
2026-02-16 21:40:11 +0300

    add googleId to user entity migrations are done

--- src/CurrencyTracker.Domain/Entities/User.cs
+++ src/CurrencyTracker.Domain/Entities/User.cs
@@ -6,7 +6,8 @@ public class User : BaseEntity
 {
     public string Username { get; set; } = string.Empty;
     public string PasswordHash { get; set; } = string.Empty;  
-    public string AuthProvider { get; set; } = "Local";
+    public string AuthProvider { get; set; } = default!;
+    public string GoogleId {get;set;} = default!;
     public string? RefreshToken { get; set; }
     public DateTime? RefreshTokenExpiryTime { get; set; }
     public string Email { get; set; } = string.Empty;
