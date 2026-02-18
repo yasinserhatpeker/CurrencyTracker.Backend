5f6e7952ecc846d3f6f7b9f819c0abf39c4e68e0
Yasin <yasinpeker43@gmail.com>
2026-02-18 21:37:08 +0300

    RefreshTokenHash added to the user entity and its migrations are done

--- src/CurrencyTracker.Domain/Entities/User.cs
+++ src/CurrencyTracker.Domain/Entities/User.cs
@@ -8,7 +8,8 @@ public class User : BaseEntity
     public DateTime? ResetPasswordTokenExpiryTime{get;set;}
     public string AuthProvider { get; set; } = default!;
     public string GoogleId {get;set;} = default!;
-    public string? RefreshToken { get; set; }
+    public string? RefreshTokenHash {get;set;}
+    public string? RefreshToken{get;set;}
     public DateTime? RefreshTokenExpiryTime { get; set; }
     public string Email { get; set; } = string.Empty;
     public ICollection<Portfolio> Portfolios { get; set; } = new List<Portfolio>();
---
+++ src/CurrencyTracker.Infrastructure/Migrations/20260218183548_addRefreshTokenHash.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CurrencyTracker.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class addRefreshTokenHash : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "RefreshTokenHash",
+                table: "Users",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RefreshTokenHash",
+                table: "Users");
+        }
+    }
+}
