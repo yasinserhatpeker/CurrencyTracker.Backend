b509f1511626dbf847862bea6d3ab3b76f9dafd4
Yasin <yasinpeker43@gmail.com>
2026-02-23 19:41:09 +0300

    IsEmailVerified and EmailVerificationTokenHash are added to the user entity

--- src/CurrencyTracker.Domain/Entities/User.cs
+++ src/CurrencyTracker.Domain/Entities/User.cs
@@ -4,12 +4,19 @@ public class User : BaseEntity
 {
     public string Username { get; set; } = string.Empty;
     public string PasswordHash { get; set; } = string.Empty;
+
     public string? ResetPasswordTokenHash {get;set;}
     public DateTime? ResetPasswordTokenExpiryTime{get;set;}
+
     public string AuthProvider { get; set; } = "Local";
     public string? GoogleId {get;set;}
+
     public string? RefreshTokenHash {get;set;}
     public DateTime? RefreshTokenExpiryTime { get; set; }
+
+    public bool IsEmailVerified {get;set;} = false;
+    public string? EmailVerificationTokenHash {get;set;}
+    
     public string Email { get; set; } = string.Empty;
     public ICollection<Portfolio> Portfolios { get; set; } = new List<Portfolio>();
 
