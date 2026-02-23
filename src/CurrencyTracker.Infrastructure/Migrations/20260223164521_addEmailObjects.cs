4d8bc76511700da56b2484ca064d183261ea77c4
Yasin <yasinpeker43@gmail.com>
2026-02-23 19:46:10 +0300

    migrations are done

---
+++ src/CurrencyTracker.Infrastructure/Migrations/20260223164521_addEmailObjects.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CurrencyTracker.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class addEmailObjects : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "EmailVerificationTokenHash",
+                table: "Users",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsEmailVerified",
+                table: "Users",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EmailVerificationTokenHash",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "IsEmailVerified",
+                table: "Users");
+        }
+    }
+}
