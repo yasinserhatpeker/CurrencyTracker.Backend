c924748b1ec013ab4c03de020ed5287b2cfe2331
Yasin <yasinpeker43@gmail.com>
2026-03-12 16:00:25 +0300

    UserAccount interface is done

--- src/CurrencyTracker.Application/Interfaces/IUserAccountService.cs
+++ src/CurrencyTracker.Application/Interfaces/IUserAccountService.cs
@@ -1,6 +1,10 @@
+using CurrencyTracker.Application.DTOs.Users;
+
 namespace CurrencyTracker.Application.Interfaces;
 
 public interface IUserAccountService
 {
-    
+    Task<bool> EmailVerificationAsync(string token);
+    Task ForgotPasswordAsync(ForgotPasswordDTO forgotPasswordDTO);
+    Task ResetPasswordAsync(ResetPasswordDTO resetPasswordDTO);
 }
