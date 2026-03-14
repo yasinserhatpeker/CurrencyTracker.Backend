9e90fccfbbcc43d764853ef10e5d773691732e0e
Yasin <yasinpeker43@gmail.com>
2026-03-14 12:15:20 +0300

    added Precision model for decimal currencies & migrations are done

---
+++ src/CurrencyTracker.Infrastructure/Migrations/20260314091330_AddPrecisionModel.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CurrencyTracker.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPrecisionModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Quantity",
+                table: "Transactions",
+                type: "numeric(18,8)",
+                precision: 18,
+                scale: 8,
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "numeric");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Price",
+                table: "Transactions",
+                type: "numeric(18,8)",
+                precision: 18,
+                scale: 8,
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "numeric");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Quantity",
+                table: "Transactions",
+                type: "numeric",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "numeric(18,8)",
+                oldPrecision: 18,
+                oldScale: 8);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Price",
+                table: "Transactions",
+                type: "numeric",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "numeric(18,8)",
+                oldPrecision: 18,
+                oldScale: 8);
+        }
+    }
+}
--- src/CurrencyTracker.Infrastructure/Persistence/AppDbContext.cs
+++ src/CurrencyTracker.Infrastructure/Persistence/AppDbContext.cs
@@ -10,7 +10,15 @@ public class AppDbContext:DbContext
     {
         
     }
-
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder); 
+        modelBuilder.Entity<Transaction>(builder =>
+        {
+            builder.Property(t=>t.Price).HasPrecision(18,8);
+            builder.Property(t=>t.Quantity).HasPrecision(18,8);         
+            });
+    }
     public DbSet<User> Users {get; set;}
     public DbSet<Portfolio> Portfolios {get; set;}
     public DbSet<Transaction> Transactions {get; set;}
