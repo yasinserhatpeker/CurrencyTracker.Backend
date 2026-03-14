9d2e8b64fb43c03da291ae4bdba7135df76cc8f9
Yasin <yasinpeker43@gmail.com>
2026-03-14 09:33:21 +0300

    fix typo

--- src/CurrencyTracker.Application/Interfaces/IGenericRepository.cs
+++ src/CurrencyTracker.Application/Interfaces/IGenericRepository.cs
@@ -10,7 +10,6 @@ public interface IGenericRepository<T> where T : class
     Task<T> AddAsync(T entity);
     Task<T> UpdateAsync(T entity);
     Task<T> DeleteAsync(Guid Id);
-   
     Task SaveAsync();
 
 }
