ef47f48489e712f029b4944431825cdaf4db1609
Yasin <yasinpeker43@gmail.com>
2026-03-14 14:40:26 +0300

    ApiResponse wrapper is done

---
+++ src/CurrencyTracker.Application/Wrappers/ApiResponse.cs
@@ -0,0 +1,36 @@
+using System;
+
+namespace CurrencyTracker.Application.Wrappers;
+
+public class ApiResponse<T>
+{
+    public T? Data { get; set; }
+    public bool IsSuccess { get; set; }
+    public string? Message { get; set; }
+    public List<string>? Errors { get; set; }
+
+
+
+    public static ApiResponse<T> Success(T data, string? message = null)
+    {
+        return new ApiResponse<T> { Data = data, IsSuccess = true, Message = message };
+    }
+
+    public static ApiResponse<T> Success(string message)
+
+    {
+        return new ApiResponse<T> { IsSuccess = true, Message = message };
+    }
+
+    public static ApiResponse<T> Fail(string error, string? message = null)
+    {
+        return new ApiResponse<T> { IsSuccess = false, Message = message, Errors = new List<string> { error } };
+    }
+
+    public static ApiResponse<T> Fail(List<string> errors, string? message = null)
+    {
+        return new ApiResponse<T>{ IsSuccess = false, Message = message, Errors = errors };
+    }
+
+    
+}
