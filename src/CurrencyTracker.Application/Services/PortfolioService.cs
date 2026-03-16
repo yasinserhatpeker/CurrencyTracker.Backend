ee70d4769359d1fa7f27439933d16d119544b7d2
Yasin <yasinpeker43@gmail.com>
2026-03-16 22:49:05 +0300

    portfolioService is refactored with serilog

--- src/CurrencyTracker.Application/Services/PortfolioService.cs
+++ src/CurrencyTracker.Application/Services/PortfolioService.cs
@@ -2,6 +2,7 @@ using AutoMapper;
 using CurrencyTracker.Application.DTOs.Portfolios;
 using CurrencyTracker.Application.Interfaces;
 using CurrencyTracker.Domain.Entities;
+using Microsoft.Extensions.Logging;
 
 
 namespace CurrencyTracker.Application.Services;
@@ -11,17 +12,21 @@ public class PortfolioService : IPortfolioService
     private readonly IMapper _mapper;
     private readonly IGenericRepository<Portfolio> _portfolioRepository;
 
-    public PortfolioService(IMapper mapper, IGenericRepository<Portfolio> portfolioRepository)
+    private readonly ILogger<PortfolioService> _logger;
+
+    public PortfolioService(IMapper mapper, IGenericRepository<Portfolio> portfolioRepository, ILogger<PortfolioService> logger)
     {
         _mapper=mapper;
         _portfolioRepository= portfolioRepository;
+        _logger=logger;
     }
     
     public async Task<PortfolioResponseDTO> CreatePortfolioAsync(CreatePortfolioDTO createPortfolioDTO)
     {
         var newPortfolio = _mapper.Map<Portfolio>(createPortfolioDTO);
-
         await _portfolioRepository.AddAsync(newPortfolio);
+
+        _logger.LogInformation("a new portfolio is created for the user {UserId} and the id of the portfolio is {Id}",createPortfolioDTO.UserId,newPortfolio.Id);
 
         return  _mapper.Map<PortfolioResponseDTO>(newPortfolio);
 
@@ -33,8 +38,10 @@ public class PortfolioService : IPortfolioService
          var deletedPortfolio = await _portfolioRepository.DeleteAsync(id);
          if(deletedPortfolio is null)
         {
+          _logger.LogWarning("a portfolio is not found. The id of the portfolio is {Id}",id);
           throw new KeyNotFoundException("Portfolio not found");
         }
+        _logger.LogInformation("a portfolio is deleted for the user {UserId} and the id of the portfolio is {Id}",deletedPortfolio.UserId, deletedPortfolio.Id);
        
 
     }
@@ -43,7 +50,8 @@ public class PortfolioService : IPortfolioService
     {
         var portfolio = await _portfolioRepository.GetByIdAsync(id);
         if(portfolio is null)
-        {
+        {   
+            _logger.LogWarning("a portfolio is not found. The id of the portfolio is {Id}",id);
             throw new KeyNotFoundException("Portfolio not found");
         }
         return _mapper.Map<PortfolioResponseDTO>(portfolio);
@@ -52,6 +60,11 @@ public class PortfolioService : IPortfolioService
     public async Task<IEnumerable<PortfolioResponseDTO>> GetPortfoliosByUserAsync(Guid userId)
     {
         var userPortfolios = await _portfolioRepository.Find(x => x.UserId == userId);
+        if(userPortfolios is null)
+        {
+            _logger.LogWarning("no portfolio is found for the user {UserId}",userId);
+            throw new KeyNotFoundException("No portfolio is found for the user");
+        }
     
         return _mapper.Map<IEnumerable<PortfolioResponseDTO>>(userPortfolios);
 
@@ -62,10 +75,14 @@ public class PortfolioService : IPortfolioService
        var portfolio = await _portfolioRepository.GetByIdAsync(id);
        if(portfolio is null)
         {
+            _logger.LogWarning("a portfolio is not found. The id of the portfolio is {Id}",updatePortfolioDTO.Id);
             throw new KeyNotFoundException("No portfolio is found");
+            
         }
             _mapper.Map(updatePortfolioDTO,portfolio);
          await _portfolioRepository.UpdateAsync(portfolio); 
+
+        _logger.LogInformation("a portfolio is updated for the user {UserId} and the id of the portfolio is {Id}",portfolio.UserId, portfolio.Id);
 
          return _mapper.Map<PortfolioResponseDTO>(portfolio);
         
