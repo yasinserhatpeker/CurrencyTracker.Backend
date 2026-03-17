f1305d42dbb6e0070527c27ae9bf9c6b50444f03
Yasin <yasinpeker43@gmail.com>
2026-03-17 12:49:49 +0300

    fix typo

--- src/CurrencyTracker.API/Middlewares/ExceptionMiddleware.cs
+++ src/CurrencyTracker.API/Middlewares/ExceptionMiddleware.cs
@@ -8,44 +8,44 @@ public class ExceptionMiddleware
     private readonly RequestDelegate _next;
     private readonly ILogger<ExceptionMiddleware> _logger;
 
-    public ExceptionMiddleware(RequestDelegate next,ILogger<ExceptionMiddleware> logger)
+    public ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger)
     {
         _next = next;
         _logger = logger;
     }
 
     public async Task InvokeAsync(HttpContext context)
-    {  
+    {
         try
         {
-        await _next(context);
-            
+            await _next(context);
+
         }
-        catch(Exception ex)
-        {   
-            _logger.LogError(ex,"An unexpected error occured in the system. Request path:{requestPath}",context.Request.Path);
-            await HandleExceptionAsync(context,ex);
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "An unexpected error occured in the system. Request path:{requestPath}", context.Request.Path);
+            await HandleExceptionAsync(context, ex);
         }
 
     }
 
     private static Task HandleExceptionAsync(HttpContext context, Exception exception)
     {
-        context.Response.ContentType ="application/json";
-        
+        context.Response.ContentType = "application/json";
 
-        var statusCode= exception switch
+
+        var statusCode = exception switch
         {
             KeyNotFoundException => (int)HttpStatusCode.NotFound, // 404
             UnauthorizedAccessException => (int)HttpStatusCode.Unauthorized, // 401
             ArgumentException => (int)HttpStatusCode.BadRequest, // 400
-            _ =>(int)HttpStatusCode.InternalServerError, // 500
+            _ => (int)HttpStatusCode.InternalServerError, // 500
 
         };
         var responseMessage = statusCode == 500 ? "An unexpected error occured in the system." : exception.Message;
-         
+
         var response = ApiResponse<object>.Fail(responseMessage);
 
-        return context.Response.WriteAsJsonAsync(response);  
+        return context.Response.WriteAsJsonAsync(response);
     }
 }
