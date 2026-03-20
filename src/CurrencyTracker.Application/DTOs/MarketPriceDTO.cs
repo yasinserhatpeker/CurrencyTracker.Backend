69ecc88a516e6d74c96131b8480d2a04319e3b2f
Yasin <yasinpeker43@gmail.com>
2026-03-20 16:09:47 +0300

    MarketPriceDTO is done

--- src/CurrencyTracker.Application/DTOs/MarketPriceDTO.cs
+++ src/CurrencyTracker.Application/DTOs/MarketPriceDTO.cs
@@ -2,7 +2,12 @@ using System;
 
 namespace CurrencyTracker.Application.DTOs;
 
-public class MarketPriceDTO
+public record MarketPriceDTO
 {
-     
+    public string Symbol { get; init; } = string.Empty;
+    public DateTime LastUpdated { get; init; }
+    public decimal? ChangePercentage24H { get; init; }
+    public string QuoteCurrency { get; init; } = string.Empty;
+    public string? Source { get; init; } = string.Empty;
+    public bool IsUp => ChangePercentage24H > 0;
 }
