5ad3bc970e3b10e4bd540f2bf96954e0d47b3ff0
Yasin <yasinpeker43@gmail.com>
2026-03-24 17:54:50 +0300

    fix typo

--- src/CurrencyTracker.Application/DTOs/Portfolios/PortfolioSummaryDTO.cs
+++ src/CurrencyTracker.Application/DTOs/Portfolios/PortfolioSummaryDTO.cs
@@ -2,20 +2,17 @@ namespace CurrencyTracker.Application.DTOs.Portfolios;
 
 public record PortfolioSummaryDTO
 {
-    public Guid Id {get;init;}
-    public Guid UserId {get;init;}   
-    public string Name {get;init;} = string.Empty;
+    public Guid Id { get; init; }
 
-    public decimal TotalInvested {get;init;} // how much you spent
-    public decimal CurrentValue {get;init;}  // what's worth it today
-
+    public Guid UserId { get; init; }
+    public string Name { get; init; } = string.Empty;
+    public decimal TotalInvested { get; init; } // how much you spent
+    public decimal CurrentValue { get; init; }  // what's worth it today
     public decimal TotalProfitLoss => CurrentValue - TotalInvested; // is it loss or profit
+    public decimal ProfitLossPercentage => TotalInvested != 0
+            ? TotalProfitLoss / TotalInvested * 100 // percentage of the profit or loss
+            : 0;
+    public bool IsProfitable => TotalProfitLoss > 0;
 
-    public decimal ProfitLossPercentage => TotalInvested != 0 
-        ? TotalProfitLoss / TotalInvested * 100 // percentage of the profit or loss
-        : 0;
 
-    public bool IsProfitable => TotalProfitLoss >0; 
-     
-    
 }
