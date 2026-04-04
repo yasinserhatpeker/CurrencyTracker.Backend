df8533085fad112ec1fa12b2d3f28f022f862908
Yasin <yasinpeker43@gmail.com>
2026-04-04 22:47:09 +0300

    add DI to coingeckoClient

--- src/CurrencyTracker.Infrastructure/ExternalApis/CoinGeckoClient.cs
+++ src/CurrencyTracker.Infrastructure/ExternalApis/CoinGeckoClient.cs
@@ -1,14 +1,23 @@
-using System;
 using CurrencyTracker.Application.DTOs;
 using CurrencyTracker.Application.Interfaces;
+using Microsoft.Extensions.Logging;
 
 namespace CurrencyTracker.Infrastructure.ExternalAPIs;
 
 public class CoinGeckoClient : IPriceProvider
 {
+    private readonly HttpClient _client;
+
+    private readonly ILogger<CoinGeckoClient> _logger;
     public string ProviderName => "CoinGecko";
 
-    public int Priority => 10;
+    public int Priority => 1;
+    
+   public CoinGeckoClient(HttpClient client , ILogger<CoinGeckoClient> logger)
+   {
+       _client = client;
+       _logger = logger;
+   }
 
     public Task<MarketPriceDTO> GetPriceAsync(string baseCurrency, string quoteCurrency)
     {
