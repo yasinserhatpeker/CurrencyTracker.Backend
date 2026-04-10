59f486908c70ff74c4a764ea79e32368c92fdbd9
Yasin <yasinpeker43@gmail.com>
2026-04-10 12:49:31 +0300

    refactor applyconfiguration with assembly

--- src/CurrencyTracker.Infrastructure/Persistence/AppDbContext.cs
+++ src/CurrencyTracker.Infrastructure/Persistence/AppDbContext.cs
@@ -1,3 +1,4 @@
+using System.Reflection;
 using CurrencyTracker.Domain.Entities;
 using CurrencyTracker.Infrastructure.Persistence.EntityConfigurations;
 using Microsoft.EntityFrameworkCore;
@@ -12,7 +13,7 @@ public class AppDbContext:DbContext
     }
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        modelBuilder.ApplyConfiguration(new TransactionConfiguration());
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
     }
     public DbSet<User> Users {get; set;}
     public DbSet<Portfolio> Portfolios {get; set;}
