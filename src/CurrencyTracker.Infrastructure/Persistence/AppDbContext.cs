636c766f2e507abac73c1e263fb63639c058433c
Yasin <yasinpeker43@gmail.com>
2026-04-10 12:44:11 +0300

    applying the configurations in appdbcontext

--- src/CurrencyTracker.Infrastructure/Persistence/AppDbContext.cs
+++ src/CurrencyTracker.Infrastructure/Persistence/AppDbContext.cs
@@ -1,4 +1,5 @@
 using CurrencyTracker.Domain.Entities;
+using CurrencyTracker.Infrastructure.Persistence.EntityConfigurations;
 using Microsoft.EntityFrameworkCore;
 
 namespace CurrencyTracker.Infrastructure.Persistence;
@@ -8,6 +9,10 @@ public class AppDbContext:DbContext
   public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
     {
         
+    }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfiguration(new TransactionConfiguration());
     }
     public DbSet<User> Users {get; set;}
     public DbSet<Portfolio> Portfolios {get; set;}
